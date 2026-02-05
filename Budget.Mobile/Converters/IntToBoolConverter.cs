8dcfd8e88340dff6fc16255e351e5b8dae8ced4f
samildanak <robin.levivier@gmail.com>
2026-02-05 17:13:28 -0500

    Ajout de IntToBoolConverter

---
+++ Budget.Mobile/Converters/IntToBoolConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace Budget.Mobile.Converters
+{
+    public class IntToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // On vérifie si la valeur reçue est un entier (int)
+            if (value is int count)
+            {
+                // Renvoie Vrai si le nombre est supérieur à 0
+                return count > 0;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
